3a09be14b9d760f8cba3716fe6a4377708df6da6
w0rng <antonabramov2000@gmail.com>
2021-07-12 14:13:39 +0700

    Подтверждение и блокировка пользователя

--- src/user/admin.py
+++ src/user/admin.py
@@ -1,7 +1,50 @@
+from django.conf.urls import url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.html import format_html
+
 from . import models
 
 
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'get_full_name', 'email', 'type')
+    list_display = ('pk', 'get_full_name', 'email', 'confirmed', 'user_actions')
+
+    def do_confirm(self, request, user_id, *args, **kwargs):
+        models.User.objects.filter(id=user_id).update(confirmed=True)
+        return HttpResponseRedirect("../../")
+
+    def do_ban(self, request, user_id, *args, **kwargs):
+        models.User.objects.filter(id=user_id).update(is_active=False)
+        return HttpResponseRedirect("../../")
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            url(
+                r'^(?P<user_id>.+)/confirm/$',
+                self.admin_site.admin_view(self.do_confirm),
+                name='user-confirm',
+            ),
+            url(
+                r'^(?P<user_id>.+)/ban/$',
+                self.admin_site.admin_view(self.do_ban),
+                name='user-ban',
+            ),
+        ]
+        return custom_urls + urls
+
+    def user_actions(self, obj):
+        if not obj.confirmed:
+            return format_html(
+                '<a class="button" href="{}">Подтвердить</a>',
+                reverse('admin:user-confirm', args=[obj.pk])
+            )
+        return format_html(
+            '<a class="deletelink" href="{}">Заблокировать</a>',
+            reverse('admin:user-ban', args=[obj.pk])
+        )
+
+    user_actions.short_description = 'Действия'
+    user_actions.allow_tags = True
