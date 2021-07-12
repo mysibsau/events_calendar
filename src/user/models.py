61cb3f07896728ff4a05ca8303cdcb287738b2dc
w0rng <antonabramov2000@gmail.com>
2021-07-12 14:12:20 +0700

    Новое поле для пользователя

--- src/user/models.py
+++ src/user/models.py
@@ -6,11 +6,7 @@ from rest_framework.authtoken.models import Token
 
 
 class User(AbstractUser):
-    CHOICES = (
-        (0, 'Участник'),
-        (1, 'Автор')
-    )
-    type = models.IntegerField('Тип', choices=CHOICES, default=0)
+    confirmed = models.IntegerField('Подтвержден', default=False)
 
     def __str__(self):
         return self.get_full_name()
