71a37033aedc7f1c98e4862f1996893cb9d4c927
w0rng <antonabramov2000@gmail.com>
2021-07-14 16:34:12 +0700

    refactor serializers

--- src/events/serializers.py
+++ src/events/serializers.py
@@ -4,36 +4,27 @@ from . import models
 
 
 class EventSerializer(serializers.ModelSerializer):
-    user = serializers.CharField(read_only=True, default=serializers.CurrentUserDefault())
-    num_participants = serializers.IntegerField(read_only=True)
-    avg_rate = serializers.FloatField(read_only=True, default=0)
-
     class Meta:
         model = models.Event
-        fields = ('name', 'start_date', 'user', 'num_participants', 'avg_rate')
+        fields = ('id', 'name', 'start_date', 'stop_date')
 
 
-class RequestSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(read_only=True, default=serializers.CurrentUserDefault())
-
+class CommentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Request
-        fields = '__all__'
-
-
-class FeedbackSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(read_only=True, default=serializers.CurrentUserDefault())
-
-    class Meta:
-        model = models.Feedback
+        model = models.Comment
         fields = '__all__'
 
 
 class EventDetailSerializer(serializers.ModelSerializer):
-    feedback_set = FeedbackSerializer(many=True, read_only=True)
-    num_participants = serializers.IntegerField(read_only=True)
-    avg_rate = serializers.FloatField(read_only=True, default=0)
+    direction = serializers.StringRelatedField(source='direction.name', read_only=True)
+    responsible = serializers.StringRelatedField(source='responsible.first_name', read_only=True)
+    organization = serializers.StringRelatedField(source='organization.name', read_only=True)
+    verified = serializers.StringRelatedField(source='verified.first_name', read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+
+    direction_id = serializers.IntegerField(write_only=True)
+    organization_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = models.Event
-        fields = ('name', 'description', 'start_date', 'user', 'num_participants', 'avg_rate', 'feedback_set')
+        fields = '__all__'
