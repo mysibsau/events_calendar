04d6099b1b133d2d8a714bb6e138b52977e4118b
w0rng <antonabramov2000@gmail.com>
2021-07-14 16:35:11 +0700

    add new urls

--- src/core/api.py
+++ src/core/api.py
@@ -1,14 +1,13 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter
-
 from events import views as event_views
+from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
 router.register('events', event_views.EventViewSet)
-router.register('my_events', event_views.MyEventViewSet, basename='my_events')
+router.register('events/(?P<month>[0-9]{2}).(?P<year>[0-9]{4})', event_views.EventViewSet)
 
 urls = [
-    path('events/<int:pk>/detail', event_views.EventDetailView.as_view()),
+    path('events/<int:pk>', event_views.EventDetailView.as_view()),
 ]
 
 __all__ = [
