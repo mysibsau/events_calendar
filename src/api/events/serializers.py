ba3ba9bc4a7e0e9bd83e3fe7fa3ff50594d15ec8
w0rng <antonabramov2000@gmail.com>
2022-05-13 13:35:59 +0700

    ! return all fields

--- src/api/events/serializers.py
+++ src/api/events/serializers.py
@@ -1,52 +1,48 @@
-from rest_framework import serializers
-
 from apps.events import models
+from rest_framework import serializers
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.PrimaryKeyRelatedField(
-        read_only=True,
-        label='Id автора'
-    )
+    author = serializers.PrimaryKeyRelatedField(read_only=True, label="Id автора")
     author_name = serializers.CharField(
-        source='author.first_name',
+        source="author.first_name",
         read_only=True,
-        label='Имя отчество автора',
+        label="Имя отчество автора",
         allow_blank=True,
     )
     text = serializers.CharField(allow_blank=False)
 
     class Meta:
         model = models.Comment
-        fields = ('id', 'author', 'author_name', 'text', 'event', 'date')
+        fields = ("id", "author", "author_name", "text", "event", "date")
 
 
 class ImportantDateSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.ImportantDate
-        fields = ('name', 'date')
+        fields = ("name", "date")
 
 
 class EventSerializer(serializers.ModelSerializer):
     # TODO: Эта дичь может возвращать none, нужно пофиксить
-    is_verified = serializers.BooleanField(read_only=True, source='verified')
+    is_verified = serializers.BooleanField(read_only=True, source="verified")
     important_dates = ImportantDateSerializer(many=True, read_only=True)
 
     class Meta:
         model = models.Event
-        fields = ('id', 'name', 'start_date', 'stop_date', 'is_verified', 'important_dates')
+        fields = "__all__"
 
 
 class EventDetailSerializer(serializers.ModelSerializer):
     responsible = serializers.CharField(read_only=True)
-    verified = serializers.StringRelatedField(source='verified.first_name', read_only=True)
+    verified = serializers.StringRelatedField(source="verified.first_name", read_only=True)
     comments = CommentSerializer(many=True, read_only=True)
-    can_edit = serializers.BooleanField(read_only=True, label='Может ли данный пользователь редактировать мероприятие')
+    can_edit = serializers.BooleanField(read_only=True, label="Может ли данный пользователь редактировать мероприятие")
     important_dates = ImportantDateSerializer(many=True, required=False)
     author = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     def create(self, validated_data: dict):
-        important_dates = validated_data.pop('important_dates', None)
+        important_dates = validated_data.pop("important_dates", None)
         event = models.Event.objects.create(**validated_data)
         for important in important_dates:
             models.ImportantDate.objects.create(event=event, **important)
@@ -54,34 +50,34 @@ class EventDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Event
-        fields = '__all__'
+        fields = "__all__"
 
 
 class DirectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Direction
-        fields = '__all__'
+        fields = "__all__"
 
 
 class LevelSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Level
-        fields = '__all__'
+        fields = "__all__"
 
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Role
-        fields = '__all__'
+        fields = "__all__"
 
 
 class FormatSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Format
-        fields = '__all__'
+        fields = "__all__"
 
 
 class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Organization
-        fields = '__all__'
+        fields = "__all__"
