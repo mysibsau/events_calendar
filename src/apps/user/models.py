51869969a43b3d2cc70cee60729f545c3ba693ee
w0rng <antonabramov2000@gmail.com>
2022-08-18 12:54:26 +0700

    ! fix role type

--- src/apps/user/models.py
+++ src/apps/user/models.py
@@ -5,18 +5,27 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
+from apps.helpers.models import enum_max_length
 
-class UserRole(models.TextChoices):
-    author = "author", "Автор"
-    moderator = "moder", "Модератор"
-    administrator = "admin", "Администратор"
+
+class UserRole(models.IntegerChoices):
+    author = 0, "Автор"
+    moderator = 1, "Модератор"
+    administrator = 2, "Администратор"
+
+
+class Invite(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    author = models.ForeignKey("User", on_delete=models.CASCADE, related_name="invites")
+    user = models.ForeignKey(
+        "User", on_delete=models.CASCADE, related_name="invited_by", null=True, blank=True, default=None
+    )
+    role = models.IntegerField("Роль", choices=UserRole.choices)
 
 
 class User(AbstractUser):
     confirmed = models.BooleanField("Подтвержден", default=False)
-    role = models.CharField(
-        "Роль", max_length=enum_max_length(UserRole), choices=UserRole.choices, default=UserRole.author
-    )
+    role = models.IntegerField("Роль", choices=UserRole.choices, default=UserRole.author)
 
     def __str__(self):
         return self.get_full_name()
