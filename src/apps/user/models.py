c41a3546c2da8e29cb7121fd3e2d54fd5ad19880
w0rng <antonabramov2000@gmail.com>
2022-08-24 15:53:09 +0700

    + get_my_invites

--- src/apps/user/models.py
+++ src/apps/user/models.py
@@ -1,4 +1,5 @@
 import uuid
+from typing import List, Optional, Set
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -11,6 +12,7 @@ class UserRole(models.IntegerChoices):
     author = 0, "Автор"
     moderator = 1, "Модератор"
     administrator = 2, "Администратор"
+    super_admin = 3, "Супер администратор"
 
 
 class Invite(models.Model):
@@ -25,6 +27,20 @@ class Invite(models.Model):
 class User(AbstractUser):
     confirmed = models.BooleanField("Подтвержден", default=False)
     role = models.IntegerField("Роль", choices=UserRole.choices, default=UserRole.author)
+
+    @staticmethod
+    def get_invites(user: "User") -> List["User"]:
+        return User.objects.filter(pk__in=Invite.objects.filter(author=user).values_list("user", flat=True))
+
+    def get_my_invites(self, role: UserRole, user: Optional["User"] = None, result: Set["User"] = None) -> Set["User"]:
+        me = user or self
+        if not result:
+            result = set()
+        if me.role == role:
+            return {me}
+        for user in User.get_invites(me):
+            result |= set(user.get_my_invites(role, user, result))
+        return result
 
     def __str__(self):
         return self.get_full_name()
