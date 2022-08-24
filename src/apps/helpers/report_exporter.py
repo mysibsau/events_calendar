e6556fe2f6391ec282ecd7afcd66ffd9b68eb6cf
w0rng <antonabramov2000@gmail.com>
2022-08-24 16:15:02 +0700

    ! fix path

--- src/apps/helpers/report_exporter.py
+++ src/apps/helpers/report_exporter.py
@@ -1,13 +1,14 @@
+import os
+
 from docxtpl import DocxTemplate
+
 from apps.events.models import Event
 
 
 def report_exporter(event_id):
     event_data = Event.objects.first()
-    template = DocxTemplate("template.docx")
-    context = {
-        "event_name": str(event_data.name)
-    }
+    template = DocxTemplate("/".join(__file__.split("/")[:-1]) + "/template.docx")
+    context = {"event_name": str(event_data.name)}
 
     template.render(context)
     template.save("new_generated.docx")
