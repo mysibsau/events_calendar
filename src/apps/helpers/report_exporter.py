a805b0e3ea07f88f6b89ba471be4b65e011fa776
Roman <bekkerrdm@gmail.com>
2024-01-27 19:12:25 +0700

    fix error - to long name file
    - add name_file_report
    - add check len(name_file_report)
    - change method open file

--- src/apps/helpers/report_exporter.py
+++ src/apps/helpers/report_exporter.py
@@ -1,14 +1,17 @@
 import os
 from docx import Document
 
-from django.http import FileResponse
+from django.http import HttpResponse
 from docxtpl import DocxTemplate
 
 from apps.events.models import Event
 
+TEMPLATE_PATH_FILE = os.path.abspath('apps\\helpers\\template.docx')
+LIMIT_NAME_FILE_LEN = 99
 
-def report_exporter(event: Event) -> FileResponse:
-    template = DocxTemplate("/".join(__file__.split("/")[:-1]) + "/template.docx")
+
+def report_exporter(event: Event) -> HttpResponse:
+    template = DocxTemplate(TEMPLATE_PATH_FILE)
     supervisor = "Заместитель директора по ВР" if event.level.lower() == "институтский" else "Начальник ОРСП"
 
     template.render(
@@ -24,8 +27,14 @@ def report_exporter(event: Event) -> FileResponse:
             "supervisor": supervisor,
         }
     )
-    template.save(f"{event.name}_отчет.docx")
-    doc = Document(f"{event.name}_отчет.docx")
+
+    name_file_report = f"{event.name}_отчет.docx"
+
+    if len(name_file_report) > LIMIT_NAME_FILE_LEN:
+        name_file_report = name_file_report[:LIMIT_NAME_FILE_LEN + 1] + '...'
+
+    template.save(name_file_report)
+    doc = Document(name_file_report)
     table = doc.tables[0]
 
     for i, organizator in enumerate(event.report.organizators.all()):
@@ -35,8 +44,15 @@ def report_exporter(event: Event) -> FileResponse:
         table.cell(i+1, 2).text = organizator.position
         table.cell(i+1, 3).text = organizator.description
 
-    doc.save(f"{event.name}_отчет.docx")
-    response = FileResponse(open(f"{event.name}_отчет.docx", "rb"))
-    os.remove(f"{event.name}_отчет.docx")
+    doc.save(name_file_report)
+
+    with open(name_file_report, "rb") as file:
+        response = HttpResponse(
+            file.read(),
+            content_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        )
+        response["Content-Disposition"] = f"attachment; filename={name_file_report}"
+
+    os.remove(name_file_report)
 
     return response
