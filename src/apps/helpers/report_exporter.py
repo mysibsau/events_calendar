8e46c4011875747258f8f5eec26bb1a37f320055
Roman <bekkerrdm@gmail.com>
2024-02-05 11:58:07 +0700

    update report_exporter
    - update file template.docx

--- src/apps/helpers/report_exporter.py
+++ src/apps/helpers/report_exporter.py
@@ -6,25 +6,36 @@ from docxtpl import DocxTemplate
 
 from apps.events.models import Event
 
-TEMPLATE_PATH_FILE = os.path.abspath('apps\\helpers\\template.docx')
+TEMPLATE_PATH_FILE = "/".join(__file__.split("/")[:-1]) + "/template.docx"  # Linux
+# TEMPLATE_PATH_FILE = os.path.abspath('apps\\helpers\\template.docx')  # Windows
 LIMIT_NAME_FILE_LEN = 99
 
 
 def report_exporter(event: Event) -> HttpResponse:
     template = DocxTemplate(TEMPLATE_PATH_FILE)
-    supervisor = "Заместитель директора по ВР" if event.level.lower() == "институтский" else "Начальник ОРСП"
+    supervisor = "Заместитель директора по ВР" if event.level.lower() == "институтский" else "Руководитель ЦСО"
+
+    table = []
+    for count, organizator in enumerate(event.report.organizators.all()):
+        human = {'id': str(count + 1),
+                 'name': organizator.name,
+                 'position': organizator.position,
+                 'description': organizator.description
+                 }
+        table.append(human)
 
     template.render(
         {
             "event_name": event.report.name,
             "organization": event.report.organization,
-            "date": f'({event.report.start_date_fact} - {event.report.stop_date_fact})',
+            "date": f'{event.report.start_date_fact} - {event.report.stop_date_fact}',
             "place": event.report.place_fact,
             "level": event.level,
             "description": event.description,
             "count_index": event.report.count_index,
             "links": event.report.links,
             "supervisor": supervisor,
+            'table': table,
         }
     )
 
@@ -34,17 +45,6 @@ def report_exporter(event: Event) -> HttpResponse:
         name_file_report = name_file_report[:LIMIT_NAME_FILE_LEN + 1] + '...'
 
     template.save(name_file_report)
-    doc = Document(name_file_report)
-    table = doc.tables[0]
-
-    for i, organizator in enumerate(event.report.organizators.all()):
-        table.add_row()
-        table.cell(i+1, 0).text = str(i+1)
-        table.cell(i+1, 1).text = organizator.name
-        table.cell(i+1, 2).text = organizator.position
-        table.cell(i+1, 3).text = organizator.description
-
-    doc.save(name_file_report)
 
     with open(name_file_report, "rb") as file:
         response = HttpResponse(
